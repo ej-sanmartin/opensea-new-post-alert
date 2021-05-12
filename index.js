123b379882963c083f60bfcd99b0d3338d2a433a
Edgar J San Martin <edgar.sanmartinjr@gmail.com>
2021-05-12 05:15:22 -0400

    Boilerplate express app code

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const path = require('path');
+
+/* Next few lines are just standard boiler plate code for an express app */
+
+// Init Express
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Setting up nodemailer to send to my email directly
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      // user: process.env.EMAIL,
+      // pass: process.env.PASSWORD
+    }
+  });
+  
+// start the server listening for requests, using deployment option's port or locally
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server is running..."));
