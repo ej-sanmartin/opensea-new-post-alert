80a08c86a363c20b1ddc167e58da561cfab55df0
Edgar J San Martin <edgar.sanmartinjr@gmail.com>
2021-05-12 14:37:37 -0400

    Adding setup information and options for various NPM packages

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const fetch = require('node-fetch');
+const download = require('image-downloader');
+const fs = require('fs');
 const path = require('path');
 
 /* Next few lines are just standard boiler plate code for an express app */
 
-// Init Express
+// Init Express with body parser
 const app = express();
-
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Setting up nodemailer to send to my email directly
 const transporter = nodemailer.createTransport({
@@ -22,8 +21,33 @@ const transporter = nodemailer.createTransport({
       user: process.env.EMAIL,
       pass: process.env.PASSWORD
     }
-  });
-  
+});
+
+// fetching from OpenSea.io API
+const openSeaAPIUrl = ``;
+const openSeaAPIOptions = { method: 'GET', headers: {Accept: 'application/json'}};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
+
+// where to save everything
+const storageDirectory = process.env.STORAGEDIRECTORY;
+
+// image downloader setup and application
+const imageDownloaderOptions = {
+  url: ``,
+  dest: storageDirectory,
+  timeout: process.env.TIMEOUT
+}
+
+download.image(imageDownloaderOptions)
+  .then(({ filename }) => {
+    console.log("Saved to ", filename)
+  })
+  .catch((err) => console.log(err));
+
 // start the server listening for requests, using deployment option's port or locally
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Server is running..."));
